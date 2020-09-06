955c965afd1109af7ee4e9d5ec8b253d1dc12126
qalintana <alintana18@gmail.com>
2020-09-06 23:08:37 +0100

    inserindo elementos

--- vetores/vetor.py
+++ vetores/vetor.py
@@ -6,6 +6,12 @@ class Vetor():
         self.__posicao = 0
 
     def inserir_elemento_posicao(self, elemento, posicao):
+        if self.__elementos[posicao] is not None:
+            vetor_inicio = self.__elementos[:posicao] + [None]
+            vetor_inicio[len(vetor_inicio) - 1] = elemento
+            vetor_final = self.__elementos[posicao:]
+            self.__elementos = vetor_inicio + vetor_final
+            self.__posicao += 1
         self.__elementos[posicao] = elemento
 
     def inserir_elemento_final(self, elemento):
