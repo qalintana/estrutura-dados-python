d6a1916a46f69aa01e4237f2694b8898a7970c1a
qalintana <alintana18@gmail.com>
2020-09-06 22:41:45 +0100

    inserindo elementos

--- main.py
+++ main.py
@@ -8,5 +8,11 @@ menu = int(input("Digite a opcão desejada: "))
 
 if menu == 1:
     vetor_teste = vetor.Vetor(3)
-    vetor_teste.inserir_elemento_posicao(1, 0)
-    print(vetor_teste.listar_elemento(0))
+    vetor_teste.inserir_elemento_final(10)
+    vetor_teste.inserir_elemento_final(4)
+    vetor_teste.inserir_elemento_final(4)
+    vetor_teste.inserir_elemento_final(4)
+    vetor_teste.inserir_elemento_final(4)
+    vetor_teste.inserir_elemento_final(4)
+    vetor_teste.inserir_elemento_final(4)
+    print(vetor_teste.listar_elemento(6))
--- vetores/vetor.py
+++ vetores/vetor.py
@@ -3,9 +3,17 @@ class Vetor():
     def __init__(self, tamanho: int):
         self.__tamanho = tamanho
         self.__elementos = [None] * tamanho
+        self.__posicao = 0
 
     def inserir_elemento_posicao(self, elemento, posicao):
         self.__elementos[posicao] = elemento
+
+    def inserir_elemento_final(self, elemento):
+        if self.__posicao >= len(self.__elementos):
+            self.__elementos = self.__elementos + [None, None]
+
+        self.__elementos[self.__posicao] = elemento
+        self.__posicao += 1
 
     def listar_elemento(self, posicao):
         return self.__elementos[posicao]
