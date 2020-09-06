b5e1948e1e119c57025ac4be7245510716876656
qalintana <alintana18@gmail.com>
2020-09-06 22:23:56 +0100

    metodo inserir e listar do vetor

--- vetores/vetor.py
+++ vetores/vetor.py
@@ -1,8 +1,11 @@
 class Vetor():
 
-    def __init__(self, tamanho):
+    def __init__(self, tamanho: int):
         self.__tamanho = tamanho
-        self.__elementos = None * tamanho
+        self.__elementos = [None] * tamanho
 
     def inserir_elemento_posicao(self, elemento, posicao):
         self.__elementos[posicao] = elemento
+
+    def listar_elemento(self, posicao):
+        return self.__elementos[posicao]
